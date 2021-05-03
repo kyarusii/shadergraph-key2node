dec5a57e27c2fb96cc432b5d3ab50e481829210f
seonghwan-dev <79823287+seonghwan-dev@users.noreply.github.com>
2021-05-03 20:37:57 +0900

    Add files via upload

---
+++ Editor/KeyboardShortcut.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UIElements;
+
+namespace ShaderGraphEnhancer
+{
+#if SGE_INSTALLED
+	using UnityEditor.ShaderGraph;
+	using UnityEditor.ShaderGraph.Drawing;
+#else
+	public class MaterialGraphView {}
+	public class GraphData { }
+
+	public class AbstractMaterialNode { }
+#endif
+
+	internal class KeyboardShortcutHelper
+    {
+        private MaterialGraphView GraphView { get; }
+        private GraphData GraphData { get; }
+ 
+        public KeyboardShortcutHelper(MaterialGraphView graphView, GraphData graph)
+        {
+#if SGE_INSTALLED
+            GraphView = graphView;
+            GraphData = graph;
+ 
+            GraphView.RegisterCallback<KeyDownEvent>(OnKeyDown);
+#endif
+        }
+ 
+        void OnKeyDown(KeyDownEvent evt) {
+#if SGE_INSTALLED
+	        
+	        
+            if (GraphData == null) return;
+ 
+            Vector2 pos = evt.originalMousePosition;
+            
+            switch (evt.keyCode) {
+                case KeyCode.O:
+                    CreateNode(() => new OneMinusNode(), pos);
+                    break;
+                case KeyCode.M:
+                    CreateNode(() => new MultiplyNode(), pos);
+                    break;
+                case KeyCode.L:
+                    CreateNode(() => new LerpNode(), pos);
+                    break;
+                case KeyCode.D:
+                    CreateNode(() => new DivideNode(), pos);
+                    break;
+                case KeyCode.S:
+                    CreateNode(() => new SubtractNode(), pos);
+                    break;
+                case KeyCode.A:
+                    CreateNode(() => new AddNode(), pos);
+                    break;
+                case KeyCode.F:
+                    CreateNode(() => new FractionNode(), pos);
+                    break;
+                case KeyCode.Alpha1:
+                    CreateNode(() => new Vector1Node(), pos);
+                    break;
+                case KeyCode.Alpha2:
+                    CreateNode(() => new Vector2Node(), pos);
+                    break;
+                case KeyCode.Alpha3:
+                    CreateNode(() => new Vector3Node(), pos);
+                    break;
+                case KeyCode.Alpha4:
+                    CreateNode(() => new Vector4Node(), pos);
+                    break;
+                case KeyCode.Alpha5:
+                    CreateNode(() => new ColorNode(), pos);
+                    break;
+            }
+#endif
+        }
+ 
+        private void CreateNode(Func<AbstractMaterialNode> createNode, Vector2 pos) {
+#if SGE_INSTALLED
+			AbstractMaterialNode multiplyNode = createNode();
+ 
+            var drawState = multiplyNode.drawState;
+            Vector2 p = GraphView.contentViewContainer.WorldToLocal(pos);
+ 
+            drawState.position = new Rect(p.x, p.y, drawState.position.width, drawState.position.height);
+            multiplyNode.drawState = drawState;
+            GraphData.AddNode(multiplyNode);
+#endif
+        }
+    }
+}
---
+++ Editor/PackageCacheOverrider.cs
@@ -0,0 +1,201 @@
+﻿using System.IO;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.Rendering;
+
+namespace ShaderGraphEnhancer
+{
+	public class PackageCacheOverrider
+	{
+		const string pre =
+			"[assembly: System.Runtime.CompilerServices.InternalsVisibleTo(\"Seonghwan.ShaderGraph.Enhancer.Editor\")]";
+
+		[InitializeOnLoadMethod]
+		private static void Override()
+		{
+			string packageCachePath = Application.dataPath.Replace("/Assets", "/Library/PackageCache");
+			DirectoryInfo packageCache = new DirectoryInfo(packageCachePath);
+
+			DirectoryInfo shaderGraphPackage = null;
+			foreach (DirectoryInfo directoryInfo in packageCache.GetDirectories())
+			{
+				if (directoryInfo.Name.Contains("com.unity.shadergraph@"))
+				{
+					shaderGraphPackage = directoryInfo;
+					break;
+				}
+				else
+				{
+					Debug.Log(directoryInfo.Name);
+				}
+			}
+
+			Assert.IsNotNull(shaderGraphPackage);
+
+			string filePath = shaderGraphPackage.FullName + "/Editor/Drawing/Views/GraphEditorView.cs";
+			FileInfo file = new FileInfo(filePath);
+
+			Assert.IsTrue(file.Exists);
+			
+			var defines =
+				PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings
+					.selectedBuildTargetGroup);
+
+			string text = File.ReadAllText(filePath);
+			string[] lines = File.ReadAllLines(filePath);
+			if (text.Contains(pre))
+			{
+				Run();
+			}
+			else
+			{
+				var removed = defines.Replace("SGE_INSTALLED", "");
+				PlayerSettings.SetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup,
+					removed);
+				
+				Install();
+			}
+			
+			
+			if(!defines.Contains("SGE_INSTALLED"))
+				PlayerSettings.SetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup,
+					defines + ";SGE_INSTALLED");
+			
+			EditorApplication.wantsToQuit += () =>
+			{
+				var defines =
+					PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings
+						.selectedBuildTargetGroup);
+				var removed = defines.Replace("SGE_INSTALLED", "");
+				PlayerSettings.SetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup,
+					removed);
+
+				return true;
+			};
+
+			void Install()
+			{
+				StringBuilder sb = new StringBuilder(text.Length + 256);
+				sb.AppendLine("#define SG_HOOKED");
+				for (int i = 0; i < lines.Length; i++)
+				{
+					if (lines[i].StartsWith("using")) { }
+				}
+
+				text = text.Replace("namespace UnityEditor.ShaderGraph.Drawing",
+							$"{pre}\n\nnamespace UnityEditor.ShaderGraph.Drawing")
+						.Replace("MaterialGraphView m_GraphView;",
+							"public static Action<MaterialGraphView, GraphData> installedCallback;\n\t\t\t\tMaterialGraphView m_GraphView;")
+						.Replace("m_InspectorView.InitializeGraphSettings();",
+							"m_InspectorView.InitializeGraphSettings();\n\t\t\t\tinstalledCallback?.Invoke(graphView, graph);")
+					;
+				File.WriteAllText(filePath, text);
+
+				var defines =
+					PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings
+						.selectedBuildTargetGroup);
+				
+				if(!defines.Contains("SGE_INSTALLED"))
+					PlayerSettings.SetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup,
+					defines + ";SGE_INSTALLED");
+			}
+
+			void Run()
+			{
+#if SGE_INSTALLED
+				UnityEditor.ShaderGraph.Drawing.GraphEditorView.installedCallback += (v, g) =>
+				{
+					KeyboardShortcutHelper keyboardShortcutHelper = new KeyboardShortcutHelper(v, g);
+				};
+#endif
+			}
+		}
+
+		[MenuItem("Tools/SGE/Force Resolve")]
+		static void ForceInstall()
+		{
+			bool yes = EditorUtility.DisplayDialog("Force Resolve", "Resolve ShaderGraph Enhancer",
+				"Resolve", "Cancel");
+
+			if (!yes) return;
+			
+			string packageCachePath = Application.dataPath.Replace("/Assets", "/Library/PackageCache");
+			DirectoryInfo packageCache = new DirectoryInfo(packageCachePath);
+
+			DirectoryInfo shaderGraphPackage = null;
+			foreach (DirectoryInfo directoryInfo in packageCache.GetDirectories())
+			{
+				if (directoryInfo.Name.Contains("com.unity.shadergraph@"))
+				{
+					shaderGraphPackage = directoryInfo;
+					break;
+				}
+				else
+				{
+					Debug.Log(directoryInfo.Name);
+				}
+			}
+
+			Assert.IsNotNull(shaderGraphPackage);
+
+			string filePath = shaderGraphPackage.FullName + "/Editor/Drawing/Views/GraphEditorView.cs";
+
+			string text = File.ReadAllText(filePath);
+			string[] lines = File.ReadAllLines(filePath);
+
+			StringBuilder sb = new StringBuilder(text.Length + 256);
+			sb.AppendLine("#define SG_HOOKED");
+			for (int i = 0; i < lines.Length; i++)
+			{
+				if (lines[i].StartsWith("using")) { }
+			}
+
+			text = text.Replace("namespace UnityEditor.ShaderGraph.Drawing",
+						$"{pre}\n\nnamespace UnityEditor.ShaderGraph.Drawing")
+					.Replace("MaterialGraphView m_GraphView;",
+						"public static Action<MaterialGraphView, GraphData> installedCallback;\n\t\t\t\tMaterialGraphView m_GraphView;")
+					.Replace("m_InspectorView.InitializeGraphSettings();",
+						"m_InspectorView.InitializeGraphSettings();\n\t\t\t\tinstalledCallback?.Invoke(graphView, graph);")
+				;
+			File.WriteAllText(filePath, text);
+
+			var defines =
+				PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings
+					.selectedBuildTargetGroup);
+			if(!defines.Contains("SGE_INSTALLED"))
+				PlayerSettings.SetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup,
+				defines + ";SGE_INSTALLED");
+
+			EditorApplication.wantsToQuit += () =>
+			{
+				var defines =
+					PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings
+						.selectedBuildTargetGroup);
+				var removed = defines.Replace("SGE_INSTALLED", "");
+				PlayerSettings.SetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup,
+					removed);
+
+				return true;
+			};
+		}
+
+		[MenuItem("Tools/SGE/Clean Uninstall")]
+		static void CleanRemove()
+		{
+			bool yes = EditorUtility.DisplayDialog("Uninstall", "Uninstalling ShaderGraph Enhancer package",
+				"Uninstall", "Cancel");
+
+			if (!yes) return;
+			
+			var defines =
+				PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings
+					.selectedBuildTargetGroup);
+			var removed = defines.Replace("SGE_INSTALLED", "");
+			PlayerSettings.SetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup,
+				removed);
+			UnityEditor.PackageManager.Client.Remove("com.unity.seonghwan.shadergraph-enhancer");
+		}
+	}
+}
